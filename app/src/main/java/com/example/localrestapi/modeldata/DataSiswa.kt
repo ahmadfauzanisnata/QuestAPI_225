7d7c0cb0e11fcaab05882a01dbdc10bb9d181073
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-12-18 09:09:04 +0700

    Tambahkan data class DetailSiswa dengan nilai default

--- app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
@@ -12,7 +12,12 @@ data class DataSiswa(
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false )
-d
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
 
 
 
