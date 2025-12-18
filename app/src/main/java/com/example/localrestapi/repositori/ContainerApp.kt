48e4f4f903b38fa5a710bf7145f95e71132a556c
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-12-18 21:37:12 +0700

    Implementasi DefaultAppContainer untuk konfigurasi Retrofit dan DI

--- app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/localrestapi/repositori/ContainerApp.kt
@@ -9,3 +9,50 @@ import retrofit2.Retrofit
 interface ContainerApp {
     val repositoryDataSiswa: RepositoryDataSiswa
 }
+
+class DefaultAppContainer : ContainerApp {
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl = baseurl)
+        .addConverterFactory(
+            factory = Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klien)
+        .build()
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(retrofitService)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
